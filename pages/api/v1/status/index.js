5e565bea614770bec55f4bbe521eb339e569ff4d
Carlos Daniel <carlosdanieldev@gmail.com>
2025-01-28 00:29:03 -0300

    feat: add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,12 @@
-function status(_, response) {
+import database from "../../../../infra/database.js";
+
+async function status(_, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log({
+    result: result.rows,
+  });
+
   response.status(200).json({ hello: "world" });
 }
 
