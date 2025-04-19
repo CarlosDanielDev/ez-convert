1e38fecd6fd19304664ca44b796de3a7aa193f77
Carlos Daniel <carlosdanieldev@gmail.com>
2025-04-19 02:20:43 -0300

    feat: adds `ServiceError` and refactor `InternalServerError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,49 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Internal Server Error", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Get help from the support team.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service Error", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Check if the service is available.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method Not Allowed");
+    this.name = "MethodNotAllowed";
+    this.action = "Check if the method is correct.";
+    this.statusCode = 405;
   }
 
   toJSON() {
