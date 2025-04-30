edbd0bcbe47f803d9d0c551f305ea82efd90f705
Carlos Daniel <carlosdanieldev@gmail.com>
2025-04-29 22:47:37 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,62 @@
+import { resolve } from "node:path";
+import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+import { ServiceError } from "infra/errors";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      cause: error,
+      message: "Error listing migrations",
+    });
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      cause: error,
+      message: "Error running migrations",
+    });
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
